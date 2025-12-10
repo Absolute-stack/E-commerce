45d495cef0dd8524caf77b58ec061a3d475e3e46
unknown <imdeabsolute@gmail.com>
2025-12-10 18:39:24 +0000

    changed user indexing behaviour

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,13 +2,34 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    cartData: { type: Object, default: {} },
+    name: {
+      type: String,
+      required: [true, 'Username is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'User email is required'],
+      trim: true,
+      index: true, // creates a normal index
+      unique: true, // optional: enforce unique emails
+    },
+    password: {
+      type: String,
+      required: [true, 'User Password is required'],
+      trim: true,
+    },
+    cartData: {
+      type: Object,
+      default: {},
+      index: true,
+    },
   },
   { minimize: false }
 );
+
+// optional: explicit unique index
+userSchema.index({ email: 1 }, { unique: true });
 
 const userModel = mongoose.models.users || mongoose.model('user', userSchema);
 
